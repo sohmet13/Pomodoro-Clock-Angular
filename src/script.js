9983be0c16a2ee9dd289f42e4d5a8a8a75eb6650
17537870 <Loshchakova.A.A@sberbank.ru>
2019-12-17 15:36:56 +0300

    add eslint

--- src/script.js
+++ src/script.js
@@ -16,35 +16,35 @@ app.controller('myCtrl', function($scope, $timeout) {
     else if (event.target.className==='inc')
       $scope.se++;
     if ($scope.head === 'Session')
-     $scope.time=$scope.se;
-   };
+      $scope.time=$scope.se;
+  };
   $scope.Break = function(event) {
     if ($scope.br>1 && event.target.className==='dec') 
       $scope.br--;
     else if (event.target.className==='inc')
       $scope.br++;
     if ($scope.head === 'Break!')
-     $scope.time=$scope.br;
+      $scope.time=$scope.br;
   };
   //функция таймера
   var timeout='';
   function Time() {
     if (Number($scope.time)){
       //заголовок и изначальные значения
-    if ($scope.head === 'Session') {
-      $scope.head = 'Session';
-      $scope.time = $scope.se;
-      $scope.fillColor = 'green';    
-    } else {
-      $scope.head = 'Break!';
-      $scope.time = $scope.br;
-      $scope.fillColor = 'red';
-    }
+      if ($scope.head === 'Session') {
+        $scope.head = 'Session';
+        $scope.time = $scope.se;
+        $scope.fillColor = 'green';    
+      } else {
+        $scope.head = 'Break!';
+        $scope.time = $scope.br;
+        $scope.fillColor = 'red';
+      }
       secs = 60 * $scope.time;
       perc =0;
-     //вычисляем новое время сессии
+      //вычисляем новое время сессии
       if($scope.time>60) {
-       hour =Math.round(($scope.time - $scope.time%60)/60);
+        hour =Math.round(($scope.time - $scope.time%60)/60);
         minute = Math.round($scope.time%60)-1;
       } else {
         minute = $scope.time-1;
@@ -52,31 +52,31 @@ app.controller('myCtrl', function($scope, $timeout) {
       second=60;
     }
     Timer();
-    }
+  }
   //сам таймер
   function Timer(){
-  let end = false;
-      if( second > 0 ) second--;
+    let end = false;
+    if( second > 0 ) second--;
+    else{second = 59;
+      if( minute > 0 ) minute--;
       else{second = 59;
-          if( minute > 0 ) minute--;
-          else{second = 59;
-               minute=59;
-              if( hour > 0 ) hour--;
-              else {end = true};
-          }
+        minute=59;
+        if( hour > 0 ) hour--;
+        else {end = true}
       }
+    }
     perc +=100/secs;
     $scope.fillHeight = perc + '%';
     if(end){
       //переключаем таймеры
       if ($scope.head === 'Session') {
-      $scope.head = 'Break!';
-      $scope.time = $scope.br;
-      $scope.fillColor = 'red';
+        $scope.head = 'Break!';
+        $scope.time = $scope.br;
+        $scope.fillColor = 'red';
       } else {
-      $scope.head = 'Session';
-      $scope.time = $scope.se;
-      $scope.fillColor = 'green';
+        $scope.head = 'Session';
+        $scope.time = $scope.se;
+        $scope.fillColor = 'green';
       }
       perc =0;
       Time();
@@ -88,7 +88,7 @@ app.controller('myCtrl', function($scope, $timeout) {
         if (second<10){
           second='0'+second;
         }
-      $scope.time=hour+':'+minute+':'+second;
+        $scope.time=hour+':'+minute+':'+second;
       } else { 
         if (second<10){
           second='0'+second;
@@ -96,8 +96,8 @@ app.controller('myCtrl', function($scope, $timeout) {
         $scope.time=minute+':'+second;
       }
       timeout = $timeout(Timer, 1000);
-     }
     }
+  }
   //событие по клику
   let isPress = false;
   $scope.TimerBS = function(){
