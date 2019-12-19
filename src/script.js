fdff2af06852a869047221118ed68361de701a8f
17537870 <Loshchakova.A.A@sberbank.ru>
2019-12-19 17:06:48 +0300

    split functions

--- src/script.js
+++ src/script.js
@@ -2,111 +2,110 @@ import angular from 'angular';
 
 import './style.scss';
 
-var app = angular.module('PomApp', []);
+const app = angular.module('PomApp', []);
 app.controller('myCtrl', function($scope, $timeout) {
-  $scope.br = 5;
-  $scope.se = 25;
-  $scope.time = $scope.se;
-  $scope.fillHeight = '0%';
+  $scope.breakValue = 5;
+  $scope.sessionValue = 25;
+  $scope.time = $scope.sessionValue;
+  $scope.fillHeight = 0;
   $scope.head = 'Session';
-  var minute, hour, second, secs, perc=0;
-  $scope.Session = function(event) {
-    if ($scope.se>1 && event.target.className==='dec')  
-      $scope.se--;
-    else if (event.target.className==='inc')
-      $scope.se++;
-    if ($scope.head === 'Session')
-      $scope.time=$scope.se;
+  $scope.timerIsOn = false;
+  let minute, hour, second, secs;
+
+  const setTime = (varName) => {
+    if ($scope.head === 'Session' && varName === 'sessionValue')
+      $scope.time = $scope.sessionValue;
+    else if ($scope.head === 'Break!' && varName === 'breakValue')
+      $scope.time = $scope.breakValue;
   };
-  $scope.Break = function(event) {
-    if ($scope.br>1 && event.target.className==='dec') 
-      $scope.br--;
-    else if (event.target.className==='inc')
-      $scope.br++;
-    if ($scope.head === 'Break!')
-      $scope.time=$scope.br;
-  };
-  //функция таймера
-  var timeout='';
-  function Time() {
-    if (Number($scope.time)){
-      //заголовок и изначальные значения
-      if ($scope.head === 'Session') {
-        $scope.head = 'Session';
-        $scope.time = $scope.se;
-        $scope.fillColor = 'green';    
-      } else {
-        $scope.head = 'Break!';
-        $scope.time = $scope.br;
-        $scope.fillColor = 'red';
-      }
-      secs = 60 * $scope.time;
-      perc =0;
-      //вычисляем новое время сессии
-      if($scope.time>60) {
-        hour =Math.round(($scope.time - $scope.time%60)/60);
-        minute = Math.round($scope.time%60)-1;
-      } else {
-        minute = $scope.time-1;
-      }
-      second=60;
+
+  const toggleTimerStyle = (header) => {
+    if ($scope.head === header) {
+      $scope.head = 'Session';
+      $scope.time = $scope.sessionValue;
+      $scope.fillClass = 'fill_green';
+    } else {
+      $scope.head = 'Break!';
+      $scope.time = $scope.breakValue;
+      $scope.fillClass = 'fill_red';
     }
-    Timer();
-  }
+  };
+
+  const setUITime = (time) => time < 10 ? `0${time}` : time;
+
+  $scope.decrement = (varName) => {
+    if ($scope[varName] > 1) {
+      $scope[varName]--;
+    }
+    setTime(varName);
+  };
+
+  $scope.increment = (varName) => {
+    $scope[varName]++;
+    setTime(varName);
+  };
+
+  //функция таймера
+  let timeout;
+  const parseNumberToMinutes = () => {
+    //заголовок и изначальные значения
+    toggleTimerStyle('Session');
+    secs = 60 * $scope.time;
+    $scope.fillHeight = 0;
+    //вычисляем новое время сессии
+    if ($scope.time > 60) {
+      hour = Math.round(($scope.time - $scope.time%60)/60);
+      minute = Math.round($scope.time%60) - 1;
+    } else {
+      minute = $scope.time - 1;
+    }
+    second = 60;
+  };
+
+  const timerEndActions = (isTimerEnd) => {
+    if (isTimerEnd) {
+      //переключаем таймеры
+      toggleTimerStyle('Break!');
+      parseNumberToMinutes();
+      decrementSeconds();
+    } else {
+      const uiHour = hour ? `${hour}:` : '';
+      const uiMinute = hour ? setUITime(minute) : minute;
+
+      $scope.time = `${uiHour}${uiMinute}:${setUITime(second)}`;
+
+      timeout = $timeout(decrementSeconds, 1000);
+    }
+  };
   //сам таймер
-  function Timer(){
+  function decrementSeconds(){
     let end = false;
     if( second > 0 ) second--;
-    else{second = 59;
+    else {
+      second = 59;
       if( minute > 0 ) minute--;
-      else{second = 59;
-        minute=59;
-        if( hour > 0 ) hour--;
-        else {end = true}
+      else {
+        second = 59;
+        minute = 59;
+        if ( hour > 0 ) hour--;
+        else {
+          end = true
+        }
       }
     }
-    perc +=100/secs;
-    $scope.fillHeight = perc + '%';
-    if(end){
-      //переключаем таймеры
-      if ($scope.head === 'Session') {
-        $scope.head = 'Break!';
-        $scope.time = $scope.br;
-        $scope.fillColor = 'red';
-      } else {
-        $scope.head = 'Session';
-        $scope.time = $scope.se;
-        $scope.fillColor = 'green';
-      }
-      perc =0;
-      Time();
-    }else{
-      if (hour) {
-        if (minute.toString().length<2) {
-          minute = '0'+minute;
-        }
-        if (second<10){
-          second='0'+second;
-        }
-        $scope.time=hour+':'+minute+':'+second;
-      } else { 
-        if (second<10){
-          second='0'+second;
-        }
-        $scope.time=minute+':'+second;
-      }
-      timeout = $timeout(Timer, 1000);
-    }
+    $scope.fillHeight += 100/secs;
+    timerEndActions(end);
   }
   //событие по клику
-  let isPress = false;
-  $scope.TimerBS = function(){
-    if (!isPress){
-      Time(); 
-    } else {
+  $scope.toggleTimer = () => {
+    if ($scope.timerIsOn){
       $timeout.cancel(timeout);
+    } else {
+      if (Number.parseInt($scope.time)) {
+        parseNumberToMinutes();
+      }
+      decrementSeconds();
     }
-    isPress = !isPress;
-    $scope.isPress= isPress; 
+    $scope.timerIsOn = !$scope.timerIsOn;
   }
 });
